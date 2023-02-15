2e16f664790cdbc74046bc58364276f92d739932
Yury Baranov <baranow.yura1961@yandex.ru>
2023-02-15 19:05:40 +0300

    mesto-0.0

---
+++ script.js
@@ -0,0 +1,61 @@
+let formElement = document.querySelector('.content .popup .popup__container');
+
+let formElementFields = formElement.querySelectorAll('.popup__container_edit-field .popup__container_edit-line');
+
+let nameInput = formElementFields[0];
+let jobInput = formElementFields[1];
+
+let nameOutput = document.querySelector('.content .profile .profile__title-grid .profile__info .profile__title-bar .profile__title');
+let jobOutput = document.querySelector('.content .profile .profile__title-grid .profile__info .profile__subtitle');
+
+// function handleFormSubmit (evt) {
+//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+//                                                 // Так мы можем определить свою логику отправки.
+//                                                 // О том, как это делать, расскажем позже.
+    
+//     // Получите значение полей jobInput и nameInput из свойства value
+//     let name = nameInput.value;
+//     let job = jobInput.value;
+//     // Выберите элементы, куда должны быть вставлены значения полей
+
+//     // Вставьте новые значения с помощью textContent
+//     nameOutput.textContent = name;
+//     jobOutput.textContent = job;
+// }
+
+// // Прикрепляем обработчик к форме:
+// // он будет следить за событием “submit” - «отправка»
+// formElement.addEventListener('submit', handleFormSubmit); 
+
+function popupSwitcher () {
+  let popup = document.querySelector('.content .popup');
+  popup.classList.toggle('popup_opened');
+}
+  
+function setDefaultForm () {
+  nameInput.value = nameOutput.textContent;
+  jobInput.value = jobOutput.textContent;
+}
+
+function handleEditButton () {  
+  setDefaultForm();
+  popupSwitcher();
+}
+
+function handleCloseButton () {
+  popupSwitcher();
+}
+
+function handleSaveButton () {
+  nameOutput.textContent = nameInput.value;
+  jobOutput.textContent = jobInput.value;
+  popupSwitcher();
+}
+
+let buttonEdit = document.querySelector('.content .profile .profile__title-grid .profile__info .profile__title-bar .profile__button-edit');
+let buttonClose = formElement.querySelector('.popup__button-close');
+let buttonSave = formElement.querySelector('.popup__container_button-save');
+
+buttonEdit.addEventListener('click', handleEditButton);
+buttonClose.addEventListener('click', handleCloseButton);
+buttonSave.addEventListener('click', handleSaveButton);
